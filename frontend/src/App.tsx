1960d1572f8247d44db546c5689d1133dbc31597
david <davidleon_06@hotmail.com>
2023-03-28 16:52:49 +0200

    Added SPA for the app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+import Navbar from "@/features/navbar";
+import Home from "@/features/home";
+import Benefits from "@/features/benefits";
+import Classes from "@/features/classes";
+import Contact from "@/features/contact";
+import { SelectedPage } from "@/utils/types";
+import Footer from "./features/footer";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <Classes setSelectedPage={setSelectedPage} />
+      <Contact setSelectedPage={setSelectedPage} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
