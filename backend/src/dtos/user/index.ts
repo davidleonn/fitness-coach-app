e9e09bacd0fb862d196ec33cc7b18f23836e0c3e
david <davidleon_06@hotmail.com>
2023-03-31 18:29:02 +0200

    Addet users dtos

---
+++ backend/src/dtos/user/index.ts
@@ -0,0 +1,6 @@
+import userInputDTO from "./user-input.dto.json" assert { type: "json" };
+import userOutputDTO from "./user-output.dto.json" assert { type: "json" };
+
+export { userInputDTO, userOutputDTO };
+export type { UserInputDTO } from "./user-input";
+export type { UserOutputDTO } from "./user-output";
