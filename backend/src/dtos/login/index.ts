cc3813aae402eb817fa486bebd609322426515c6
david <davidleon_06@hotmail.com>
2023-04-11 16:24:46 +0200

    Moddified dtos for login

---
+++ backend/src/dtos/login/index.ts
@@ -0,0 +1,6 @@
+import userInputDTO from "./login-input.dto.json" assert { type: "json" };
+import userOutputDTO from "./login-output.dto.json" assert { type: "json" };
+
+export { userInputDTO, userOutputDTO };
+export type { LoginInputDTO } from "./login-input";
+export type { LoginOutputDTO } from "./login-output";
--- backend/src/dtos/user/index.ts
+++
@@ -1,6 +0,0 @@
-import userInputDTO from "./user-input.dto.json" assert { type: "json" };
-import userOutputDTO from "./user-output.dto.json" assert { type: "json" };
-
-export { userInputDTO, userOutputDTO };
-export type { UserInputDTO } from "./user-input";
-export type { UserOutputDTO } from "./user-output";
