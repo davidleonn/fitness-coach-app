b2b390b47c6f56bcf13025807883a2b1eef9f2b6
david <davidleon_06@hotmail.com>
2023-04-11 16:39:20 +0200

    Added users dtos for register

--- backend/src/dtos/login/index.ts
+++ backend/src/dtos/login/index.ts
@@ -1,6 +1,6 @@
-import userInputDTO from "./login-input.dto.json" assert { type: "json" };
-import userOutputDTO from "./login-output.dto.json" assert { type: "json" };
+import loginInputDTO from "./login-input.dto.json" assert { type: "json" };
+import loginOutputDTO from "./login-output.dto.json" assert { type: "json" };
 
-export { userInputDTO, userOutputDTO };
+export { loginInputDTO, loginOutputDTO };
 export type { LoginInputDTO } from "./login-input";
 export type { LoginOutputDTO } from "./login-output";
